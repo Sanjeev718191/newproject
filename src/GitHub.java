e208ef036915367716851d27664036ee6d315207
sanjeev <sanjeev718191@gmail.com>
2021-09-28 15:54:54 +0530

    Created Constructor

--- src/GitHub.java
+++ src/GitHub.java
@@ -1,7 +1,16 @@
 public class GitHub
 {
+    int a;
+    String Name;
+    GitHub()
+    {
+        a = 45;
+        Name = "Sanjeev";
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to GITHUB");
-
+        GitHub obj = new GitHub();
+        System.out.println(obj.a);
+        System.out.println(obj.Name);
     }
 }
